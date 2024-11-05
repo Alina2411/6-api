bb05cf299c309dbe09173db266448e62bd9397aa
Alina2411 <127033529+Alina2411@users.noreply.github.com>
2024-11-05 18:51:26 +0300

    Исправила ошибки

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ import os
 
 
 def download_images(filename, url, params=None):
-  response = requests.get(url, params=params)
-  response.raise_for_status()
-  with open(filename, 'wb') as file:
-      file.write(response.content)
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    with open(filename, 'wb') as file:
+       file.write(response.content)
 
 
-def get_all_number_comics():
+def get_numbers_comics():
     comics = 'https://xkcd.com/info.0.json'
     response = requests.get(comics)
     response.raise_for_status()
-    all_comic = response.json()['num']
-    return all_comic
+    total_number_comic = response.json()['num']
+    return total_number_comic
 
 
 def get_random_comics(all_number_comics):
@@ -26,21 +26,25 @@ def get_random_comics(all_number_comics):
     response = requests.get(comic_url)
     response.raise_for_status()
     comic = response.json()
-    img = comic['img']
+    img_url = comic['img']
     comment = comic["alt"]
-    return img, comment
+    return img_url, comment
     
+
 def main():
     load_dotenv()
     tg_token = os.environ['TG_TOKEN']
     chat_id = os.environ['TG_CHAT_ID']
-    bot = telegram.Bot(token=tg_token)
-    all_number_comics = get_all_number_comics()
-    img, comment = get_random_comics(all_number_comics)
-    download_images("comic.png", img)
-    with open("comic.png", 'rb') as f:
-        bot.send_photo(chat_id=chat_id, photo=f, caption=comment)
-    os.remove("comic.png") 
+    try:
+        bot = telegram.Bot(token=tg_token)
+        numbers_comics = get_numbers_comics()
+        img, comment = get_random_comics(numbers_comics)
+        download_images("comic.png", img)
+        with open("comic.png", 'rb') as f:
+            bot.send_photo(chat_id=chat_id, photo=f, caption=comment)
+    finally:
+        os.remove("comic.png") 
+
 
 if __name__ == '__main__':
     main()
